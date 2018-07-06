dec61052d5c423145462bc8ed233e6fe06f5d2f3
Yi Wang <yi.wang.2005@gmail.com>
2018-07-06 15:36:41 -0700

    Update blas.h

--- paddle/fluid/framework/math/blas.h
+++ paddle/fluid/framework/math/blas.h
@@ -31,20 +31,6 @@
 #endif
 #endif
 
-#ifndef LAPACK_FOUND
-extern "C" {
-#include <cblas.h>  // NOLINT
-int LAPACKE_sgetrf(int matrix_layout, int m, int n, float* a, int lda,
-                   int* ipiv);
-int LAPACKE_dgetrf(int matrix_layout, int m, int n, double* a, int lda,
-                   int* ipiv);
-int LAPACKE_sgetri(int matrix_layout, int n, float* a, int lda,
-                   const int* ipiv);
-int LAPACKE_dgetri(int matrix_layout, int n, double* a, int lda,
-                   const int* ipiv);
-}
-#endif
-
 namespace paddle {
 namespace fluid {
 namespace framework {
